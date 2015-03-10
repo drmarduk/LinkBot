296c51ef2ee4bb9ffd7e1d186191bf311c5783f3
raphael <raphael.elm@kelterei-elm.de>
2015-03-10 03:43:52 +0100

    added a statistic module

--- http.go
+++ http.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/thoas/stats"
 )
 
-func StartHttp() {
+func StartHttp2() {
 	mux := http.NewServeMux()
 	// routes
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/static/", staticHandler)
 
-	go http.ListenAndServe(*srvAdress+":80", http.RedirectHandler("https://"+*srvAdress, 301)) // http -> https redirect
 	http.ListenAndServeTLS(*srvAdress+":443", "data/server.crt", "data/server.key", mux)
+}
+
+func StartHttp() {
+	middleware := stats.New()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/static/", staticHandler)
+	mux.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		b, _ := json.Marshal(middleware.Data())
+		w.Write(b)
+	})
+
+	handler := middleware.Handler(mux)
+	go http.ListenAndServe(*srvAdress+":80", http.RedirectHandler("https://"+*srvAdress, 301)) // http -> https redirect
+	http.ListenAndServeTLS(*srvAdress+":443", "data/server.crt", "data/server.key", handler)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -54,4 +71,8 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 // Handler for static css/js files
 func staticHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "html/"+r.URL.Path[1:])
+}
+
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+
 }
