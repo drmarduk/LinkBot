ee21889a90ac8e699fe47dff2767f04b16bc4e00
raphael <raphael.elm@kelterei-elm.de>
2015-04-04 17:39:03 +0200

    A basic Api for searching and listing link is added. There might be some improvements later.

--- http_api.go
+++ http_api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"io"
 	"log"
@@ -17,7 +18,9 @@ var linksperpage int = 30
 func StartApi() {
 	middleware := stats.New()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/get/", getLinkHandler)
+	mux.HandleFunc("/get/", getApiHandler)
+	mux.HandleFunc("/link/", linkApiHandler)
+	mux.HandleFunc("/search/", searchApiHandler)
 	mux.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
 		b, _ := json.Marshal(middleware.Data())
 		w.Write(b)
@@ -33,10 +36,14 @@ type Response struct {
 }
 
 type LinkResult struct {
-	Id   int
-	User string
-	Url  string
-	Time time.Time
+	Id     int       `json:"id"`
+	User   string    `json:"user"`
+	Url    string    `json:"url"`
+	Time   time.Time `json:"time"`
+	Post   string    `json:"post,omitempty"`
+	Mime   string    `json:"mime,omitempty"`
+	Header string    `json:"header,omitempty"`
+	Src    string    `json:"src,omitempty"`
 }
 
 /*
@@ -50,7 +57,7 @@ type LinkResult struct {
 
 	- /search/das+ist+ein+suchtext	<- suche nach "das ist ein suchtext" -> pipe to elasticsearch
 */
-func getLinkHandler(w http.ResponseWriter, r *http.Request) {
+func getApiHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	path = strings.Replace(path, "/get/", "", 1)
 
@@ -108,6 +115,131 @@ func getLinkHandler(w http.ResponseWriter, r *http.Request) {
 			log.Println("Error while scanning row: " + err.Error())
 			continue
 		}
+		result.Data = append(result.Data, x)
+	}
+
+	result.Status = 200
+
+	x, _ := json.Marshal(result)
+	io.WriteString(w, string(x))
+}
+
+func linkApiHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	id, err := strconv.Atoi(strings.Replace(path, "/link/", "", 1))
+	if err != nil {
+		io.WriteString(w, "sorry, error")
+		return
+	}
+
+	var result Response = Response{}
+	result.Data = make([]LinkResult, 0)
+
+	db := Db{}
+	db.Open()
+	query := "select id, user, url, time, post, mime, header, src from links where id = $1"
+	err = db.Prepare(query)
+	if err != nil {
+		db.Close()
+		log.Println("Error while preparing query: " + err.Error())
+		io.WriteString(w, "sorry, error")
+		return
+	}
+
+	err = db.QueryStmt(id)
+	if err != nil { // 642058
+		db.Close()
+		log.Println("Error while query: " + err.Error())
+		io.WriteString(w, "sorry, error")
+		return
+	}
+
+	for db.ResultRows.Next() {
+		var id int
+		var timestamp time.Time
+		var nsUser, nsUrl, nsPost, nsMime, nsHeader, nsSrc sql.NullString
+
+		err = db.ResultRows.Scan(&id, &nsUser, &nsUrl, &timestamp, &nsPost, &nsMime, &nsHeader, &nsSrc)
+
+		if err != nil {
+			log.Println("Error while scanning row: " + err.Error())
+			continue
+		}
+
+		var x LinkResult = LinkResult{}
+		x.Id = id
+		if x.User = "anon"; nsUser.Valid {
+			x.User = nsUser.String
+		}
+		x.Url = nsUrl.String
+		x.Time = timestamp
+		if x.Post = ""; nsPost.Valid {
+			x.Post = nsPost.String
+		}
+		if x.Mime = ""; nsMime.Valid {
+			x.Mime = nsMime.String
+		}
+		if x.Header = ""; nsHeader.Valid {
+			x.Header = nsHeader.String
+		}
+		if x.Src = ""; nsSrc.Valid {
+			x.Src = nsSrc.String
+		}
+		result.Data = append(result.Data, x)
+	}
+
+	result.Status = 200
+
+	x, _ := json.Marshal(result)
+	io.WriteString(w, string(x))
+}
+
+func searchApiHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	term := strings.Replace(path, "/search/", "", 1)
+
+	var result Response = Response{}
+	result.Data = make([]LinkResult, 0)
+
+	db := Db{}
+	db.Open()
+	query := "select id, user, url, time from links where src like '%$1%';" //instr(src, $1) > 0"
+	err := db.Prepare(query)
+	if err != nil {
+		db.Close()
+		log.Println("Error while preparing query: " + err.Error())
+		io.WriteString(w, "sorry, error")
+		return
+	}
+
+	err = db.QueryStmt(term)
+	if err != nil {
+		db.Close()
+		log.Println("Error while query: " + err.Error())
+		io.WriteString(w, "sorry, error")
+		return
+	}
+
+	for db.ResultRows.Next() {
+		var id int
+		var timestamp time.Time
+		var nsUser, nsUrl sql.NullString
+
+		err = db.ResultRows.Scan(&id, &nsUser, &nsUrl, &timestamp)
+
+		if err != nil {
+			log.Println("Error while scanning row: " + err.Error())
+			continue
+		}
+
+		var x LinkResult = LinkResult{}
+		x.Id = id
+		if x.User = "anon"; nsUser.Valid {
+			x.User = nsUser.String
+		}
+		x.Url = nsUrl.String
+		x.Time = timestamp
+
 		result.Data = append(result.Data, x)
 	}
 
